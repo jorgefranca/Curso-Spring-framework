3e39d1df3063f2ee0ba5cecb5b44dd98af8e2f30
Jorge <jorgeartur.f@gmail.com>
2021-11-19 09:07:02 -0300

    Primeira aula

---
+++ README.md
@@ -0,0 +1,1 @@
+ Formação Spring Framework - Alura
---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundo.java
@@ -0,0 +1,33 @@
+//Essa classe é um servlet
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundo extends HttpServlet{
+	
+	//método que atende uma requisição http
+	@Override //sobrescrever o método service da classe httpservlet
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		//o out imprime para o fluxo de resposta para o navegador
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Oi, seu primeiro servlet foi feito");
+		out.println("</body>");
+		out.println("</html>");
+		
+		System.out.println("O servlet foi chamado");
+		
+	}
+
+	
+}
